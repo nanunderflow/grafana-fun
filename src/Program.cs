fa9a8ccd2f0e725af4e9d1adc61c90223fe600a8
fitz <nanunderflow@gmail.com>
2020-09-16 21:22:05 -0400

    app

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
