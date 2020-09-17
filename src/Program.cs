c269e10d26f76de9a85f5f186254fc21ef7ab623
fitz <nanunderflow@gmail.com>
2020-09-16 21:30:02 -0400

    metrics

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Core;
 
 namespace Metrics
 {
-    class Program
+    [Measurement("temperature")]
+    public class Temperature
     {
-        static void Main(string[] args)
+        [Column("location", IsTag = true)]
+        public string Location { get; set; }
+
+        [Column("value")]
+        public double Value { get; set; }
+
+        [Column(IsTimestamp = true)]
+        public DateTime Time;
+    }
+
+    public static class Program
+    {
+        private static readonly char[] Token = "".ToCharArray();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var influxDBClient = InfluxDBClientFactory.Create("http://localhost:8086", Token);
+
+            using (var writeApi = influxDBClient.GetWriteApi())
+            {
+                var temperature = new Temperature {Location = "south", Value = 62D, Time = DateTime.UtcNow};
+                writeApi.WriteMeasurement("bucket_name", "org_id", WritePrecision.Ns, temperature);
+            }
         }
     }
 }
